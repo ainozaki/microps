d6bd67c19c53489a823cfb64c5b54197a5124b4f
ainnooo <aiai10piano@gmail.com>
2022-03-04 12:29:14 +0900

    [step6] parse ip

--- ip.h
+++ ip.h
@@ -1,6 +1,15 @@
 #ifndef IP_H
 #define IP_H
 
+#include <stddef.h>
+#include <stdint.h>
+
+typedef uint32_t ip_addr_t;
+
 int ip_init(void);
 
+int ip_addr_pton(const char* p, ip_addr_t* n);
+char* ip_addr_ntop(ip_addr_t n, char* p, size_t size);
+
+void ip_dump(const uint8_t* data, size_t len);
 #endif
